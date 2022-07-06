ea7d5392b2d7b58dbdec3161c28e95c0832ffa17
yaseen2050 <arabicinnovation1@gmail.com>
2022-07-06 11:52:25 -0400

    I create new packge and new class and i print Hi guy's

---
+++ day02/HelloWorld.java
@@ -0,0 +1,7 @@
+package day02;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" Hi guy's ");
+    }
+}
---
+++ out/production/AhmedYaseen/README.md
@@ -0,0 +1,5 @@
+# AhmedYaseen
+Ahmed Yaseen
+
+this project created by yaseen to learn gitHub and git
+
