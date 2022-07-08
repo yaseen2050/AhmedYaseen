735e43bab03b880a5b708cf92e857c879660ae5a
yaseen2050 <108507664+yaseen2050@users.noreply.github.com>
2022-07-07 21:21:13 -0400

    Update HelloWorld.java

--- day02/HelloWorld.java
+++ day02/HelloWorld.java
@@ -3,5 +3,6 @@ package day02;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" Hi guy's ");
+        System.out.println(" Hi guy's ");
     }
 }
