741a8a8c2b422ef63fbafe2c6be834f8a07828dc
yaseen2050 <108507664+yaseen2050@users.noreply.github.com>
2022-07-08 01:12:02 -0400

    Update passwordclasss1.java

--- password/passwordclasss1.java
+++ password/passwordclasss1.java
@@ -4,6 +4,9 @@ public class passwordclasss1 {
     public static void main(String[] args) {
         System.out.println("this is a test");
                 System.out.println("this is a testffffffffffffffffffff");
+                        System.out.println("this is a testffffffffffffffffffff");
+                System.out.println("this is a testffffffffffffffffffff");
+
 
     }
 }
